b2d3828d9b6825e0e69a5ff2f99684ff95464914
Quan Minh Le <s3237586@yallara.cs.rmit.edu.au>
2012-08-12 22:05:34 +1000

    added dropdown lists + more fields + submit btn

--- partA.php
+++ partA.php
@@ -9,40 +9,49 @@ if (!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
 	echo 'Could not connect to mysql on ' . DB_HOST . "\n";
 	exit;
 }
-echo 'Connected to mysql on ' . DB_HOST . "\n";
+//echo 'Connected to mysql on ' . DB_HOST . "\n";
 if (!mysql_select_db(DB_NAME, $dbconn)) {
 	echo 'Could not use database ' . DB_NAME . "\n";
 	echo mysql_error() . "\n";
 exit;
 }
-echo 'Connected to database ' . DB_NAME . "\n";
-
-
-$region = mysql_query("SELECT * FROM region");
-
-while($row = mysql_fetch_array($region))
-  {
- // echo $row['region_id'] . " " . $row['region_name'];
- // echo "\n";
-  }
-
-$grape_v = mysql_query("SELECT * FROM grape_variety");
-
-while($row = mysql_fetch_array($grape_v))
-  {
- // echo $row['variety_id'] . " " . $row['variety'];
- // echo "\n";
-  }
-
-
-
-
+//echo 'Connected to database ' . DB_NAME . "\n";
+    echo "<h2>Wine Search Page</h2>";
     echo "<form action='' method='post' name='wineryform'>";
     echo "<table width='100%'>";
     echo "<tr><td>Wine Name:</td><td> <input class='text' name='winename' type='text'  /></td></tr>";
     echo "<tr><td>Winery Name:</td><td> <input class='text' name='wineryname' type='text'  /></td></tr>";
+
+	$region = mysql_query("SELECT * FROM region");
+	echo "<tr><td>Region ";
+	echo "<select name='region'>";
+	while($row = mysql_fetch_array($region))
+  	{
+	echo "<option value='" .$row['region_name'] . "'>" .$row['region_name'] ."</option>";	
+ 	// echo $row['region_id'] . " " . $row['region_name'];
+ 	// echo "\n";
+  	}
+	echo "</select></td></tr>";
+	
+	$grape_v = mysql_query("SELECT * FROM grape_variety");
+	echo "<tr><td>Grape Variety: ";
+	echo "<select name='grape_v'>";
+	while($row = mysql_fetch_array($grape_v))
+  	{
+	echo "<option value='" .$row['variety'] . "'>" .$row['variety'] ."</option>";
+	// echo $row['variety_id'] . " " . $row['variety'];
+ 	// echo "\n";
+  	}
+	echo "</select></td></tr>";
+
+    echo "<tr><td>Minimum number of wines in stock:</td><td> <input class='text' name='wineinstock' type='text'  /></td></tr>";
+    echo "<tr><td>Minimum number of wines ordered:</td><td> <input class='text' name='wineordered' type='text'  /></td></tr>";
+    echo "<tr><td>Dollar cost range: </td><td> <input class='text' name='dcr' type='text'  /></td></tr>";
+    echo "<tr><td colspan='2' align='center'><input class='text' type='submit' name='submitBtn' value='Submit' /></td></tr>";
+
+
+
 ?>
 </body>
 </head>
 </html>
-
